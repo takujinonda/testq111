446f4d03a665f71093963122486e0532944bc155
TAKUJI NODA <takujinoda@TAKUJInoMacBook-Pro.local>
2018-07-22 15:07:47 +0900

    changed main using batch_shipyard

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 from platform import python_version
 from flask import Flask
+import os
+import subprocess
+
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
+    cmd = os.environ['BATCH_SHIPYARD_CMD']
+    stdout = subprocess.check_output(cmd)
+    print(stdout)
     return 'Hello, World!'
     #return python_version()
 
