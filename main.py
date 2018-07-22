74a3db69510b6b60767c0e4100f9be331e33b268
TAKUJI NODA <takujinoda@TAKUJInoMacBook-Pro.local>
2018-07-22 12:30:39 +0900

    first commit

---
+++ main.py
@@ -0,0 +1,13 @@
+from platform import python_version
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    #  return 'Hello, World!'
+    return python_version()
+
+
+if __name__ == '__main__':
+    app.run()
